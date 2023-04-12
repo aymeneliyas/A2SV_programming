355af40126b9b3750ef852f581e9f0f75d3f1c2b
aymeneliyas <88336016+aymeneliyas@users.noreply.github.com>
2023-04-12 20:35:55 +0300

    added all-paths-from-source-to-target.python

---
+++ all-paths-from-source-to-target.py
@@ -0,0 +1,14 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        ans = []
+        def dfs(node,path):            
+            if node == len(graph) - 1:
+                ans.append(path[:])
+
+            for neighbour in graph[node]:  
+                path.append(neighbour)                  
+                dfs(neighbour,path)
+                path.pop()
+                
+        dfs(0, [0])
+        return ans
