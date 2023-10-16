fcbdf8eed5f86838f1ba24481357e6f26f061cf9
Aymen Eliyas <88336016+aymeneliyas@users.noreply.github.com>
2023-10-16 17:54:25 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,26 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def inbound(row,col):
-            return 0 <= row < m and 0 <= col < n
-        
-        direction = [(1,0),(0,1)]
-        def solve(row,col,dic):
-            if row == m-1 and col == n-1:
-                return 1
-            
-            if (row,col) in dic:
-                return dic[(row,col)]
-            tot = 0
-            for r,c in direction:
-                newrow = r + row
-                newcol = c + col
-
-                if inbound(newrow,newcol):
-                    val = solve(newrow,newcol,dic)
-                    tot += val     
-                
-            dic[(row,col)] = tot
-            return tot
-        
-        res = solve(0,0,{})
-        return res
+        return factorial(n+m-2) //( factorial(n-1) * factorial(m-1))
