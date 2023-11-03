de1ba0e1f0a4f4e0e1a39e7e9b21503bf557cfdd
Aymen Eliyas <88336016+aymeneliyas@users.noreply.github.com>
2023-11-03 16:05:29 +0300

    Added solution for Best Time to Buy and Sell Stock III

---
+++ aftercamp/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+        def rec(index,buy,tran):
+            if index >= len(prices):
+                return 0
+            if (index,buy,tran) in dp:
+                return dp[(index,buy,tran)]
+                
+            if tran <= 2:
+                pas = rec(index+1,buy,tran)
+                if buy:
+                    val = rec(index+1,not buy,tran+1) - prices[index] 
+                    dp[(index,buy,tran)] = max(pas,val)
+                
+                else:
+                    val = rec(index+1,not buy,tran) + prices[index]
+                    dp[(index,buy,tran)] = max(pas,val)
+                return dp[(index,buy,tran)]
+            else:
+                return 0
+        
+        return rec(0,True,0)
