e449c4e91394e72d731eb86973864970f6f74b47
Aymen Eliyas <88336016+aymeneliyas@users.noreply.github.com>
2023-11-07 22:18:19 +0300

    Added solution for Monotonic Array

---
+++ aftercamp/monotonic-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        rev=sorted(nums,reverse=True)
+        so=sorted(nums)
+
+        return nums==so or nums==rev
+        
